6465dd19e787537c11a2de67851c18ba8a0362ed
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-04 00:30:56 +0000

    Add pyspark ML to train models

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,2 +1,18 @@
 from pyspark.sql import SparkSession
 from prefect import task
+from pyspark.ml.feature import VectorAssembler, Imputer, StandardScaler
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import (
+    LogisticRegression, RandomForestClassifier, GBTClassifier
+)
+from pyspark.ml.evaluation import (
+    BinaryClassificationEvaluator
+)
+from pyspark.ml.feature import StringIndexer, OneHotEncoder
+
+# MLflow (Tracking, Model registry, etc)
+import mlflow
+import mlflow.spark
+from mlflow import MlflowClient
+
+
